a4d74d0faa743405b0c8e807a5d9566356ed3880
kyllesubrian <subriankylle@gmail.com>
2026-05-21 19:01:46 +0800

    change to swagger.yaml

--- src/_helpers/swagger.ts
+++ src/_helpers/swagger.ts
@@ -1,9 +1,10 @@
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
+import YAML from 'yaml';
 
-const swaggerDocument = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '../../swagger.json'), 'utf8')
+const swaggerDocument = YAML.parse(
+    fs.readFileSync(path.join(__dirname, '../../swagger.yaml'), 'utf8')
 );
 
 export const swaggerRouter = swaggerUi.setup(swaggerDocument);
